b991286a86f897ea3ebe3bd4716e9f493a437400
Quen DP <delpilarquen@gmail.com>
2023-01-31 16:32:59 +0800

    Add TestingArea.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import TestingArea from './components/TestingArea'
 function App() {
 
   return (
     <div>
+      <TestingArea />
     </div>
   )
 }
---
+++ client/src/components/TestingArea.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './TestingArea.css'
+
+const TestingArea = () => {
+  return (
+    <div className='testingArea-wrapper vh-100 d-flex flex-column justify-content-center align-items-center'>
+      <button className='cta-dark rounded-pill mb-3' >
+        Click here for Sir Roland
+      </button>
+      <button className='cta-dark rounded-pill mb-3'>
+        Click here for Sir Nherwin
+      </button>
+      <button className='cta-dark rounded-pill mb-3'>
+        Click here for Ms. Radilyn
+      </button>
+      <button className='cta-dark rounded-pill mb-3'>
+        Click here for Ms. CE
+      </button>
+      <button className='cta-dark rounded-pill mb-3'>
+        Click here for Sir Nick
+      </button>
+    </div>
+  )
+}
+
+export default TestingArea
