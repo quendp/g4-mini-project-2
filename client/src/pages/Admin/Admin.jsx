05ff1393d97fa0c315034736fdf855f3d8d3ac9c
Quen DP <delpilarquen@gmail.com>
2023-02-14 16:37:49 +0800

    #23-1-a resolve merge conflict in Admin.jsx component

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Admin.css'
+import AdminNavigations from './AdminNavigations/AdminNavigations'
 
 const Admin = () => {
   return (
-    <div>
-      Admin page for sir nherwin
+    <div className='AdminBody'>
+      <AdminNavigations />
     </div>
   )
 }
