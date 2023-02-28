b41b2ada174e5ad043d8bebd0c8d14fac16b007b
Unknick1996x <nickanthony.escobido@gmail.com>
2023-03-01 01:41:09 +0800

    27-2, adding unfinished modal

--- client/src/pages/Categories/Categories.jsx
+++ client/src/pages/Categories/Categories.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Categories.module.css";
 import { categoriesInfo } from "../../Data/CategoriesMockData";
 
 const Categories = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
   const categoryItem = categoriesInfo.find((item) => item.categoryId === 1);
   const destination = categoryItem.destinations;
 
@@ -39,22 +51,79 @@ const Categories = () => {
               >
                 {destination.map((destinationItem) => (
                   <div
+                    type="button"
+                    onClick={toggleModal}
                     key={destinationItem.id}
                     className={`${styles.catImageHolder} col-6 col-sm-4 p-2 p-md-3 m-0`}
                   >
                     <img
                       src={destinationItem.destinationImage}
-                      alt="Star City Amusement Park"
+                      alt={destinationItem.destination}
                     />
+                    {modal && (
+                      <div
+                        onClick={toggleModal}
+                        className={`${styles.modal} position-absolute top-0 start-0 h-100 w-100 p-3 p-md-4 p-lg-5`}
+                        id={`${styles.catBookInfo}`}
+                      >
+                        <div
+                          className={`${styles.overlay} d-flex h-100 w-100 flex-column justify-content-between align-items-start`}
+                        >
+                          <div
+                            key={destinationItem.id}
+                            className={`${styles.modalContent}`}
+                          >
+                            <p className="mb-1 mb-sm-3 mb-md-4">
+                              DESTINATION :
+                              <span
+                                className="ms-2"
+                                id={`${styles.catBookDestination}`}
+                              >
+                                {destinationItem.destination}
+                              </span>
+                            </p>
+                            <p className="mb-1 mb-sm-3 mb-md-4">
+                              TRIP DETAILS :
+                              <span
+                                className="ms-2"
+                                id={`${styles.catBookDetails}`}
+                              >
+                                {destinationItem.details}
+                              </span>
+                            </p>
+                            <p className="mb-1 mb-sm-3 mb-md-4">
+                              DURATION :
+                              <span
+                                className="ms-2"
+                                id={`${styles.catBookDuration}`}
+                              >
+                                {destinationItem.duration}
+                              </span>
+                            </p>
+                            <p>
+                              PRICE :
+                              <span
+                                className="ms-2"
+                                id={`${styles.catBookPrice}`}
+                              >
+                                {destinationItem.price}
+                              </span>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 ))}
               </div>
             </div>
           </div>
+
           <div className="row m-0 p-0 justify-content-center align-items-center">
             <p
               className="text-uppercase text-center"
               id={`${styles.catHeroChosen}`}
+              styles={categoryItem.accentLight}
             >
               CHOOSE A MAIN DESTINATION
             </p>
