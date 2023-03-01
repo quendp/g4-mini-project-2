4334007dabd965df541d8298946b1935b0007742
Nher10 <nherwinostia2@gmail.com>
2023-03-01 23:07:01 +0800

    #29-1 Refactored Code of AgentPage

--- client/src/pages/Agent/AgentDashboard/AgentDashComponents/AgentDashBoardLatestUpdates.jsx
+++ client/src/pages/Agent/AgentDashboard/AgentDashComponents/AgentDashBoardLatestUpdates.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import './AgentDashBoardLatestUpdates.css'
-
+import React from "react";
 
 const AgentDashBoardLatestUpdates = (props) => {
-
-
   return (
-    
-    <div className= {(props.status === 'Waitlist')? 'card agentDashBoardLatestUpdates-cards my-1 text-bg-primary':
-                     (props.status === 'Tentative')? 'card agentDashBoardLatestUpdates-cards cards my-1 text-bg-secondary':
-                     (props.status === 'Confirmed')? 'card agentDashBoardLatestUpdates-cards my-1 text-bg-success': 
-                     'card agentDashBoardLatestUpdates-cards my-1 text-bg-danger'
-                    } >
-      
+    <div
+      className={
+        props.status === "Waitlist"
+          ? "card agentDashBoardLatestUpdates-cards my-1 text-bg-primary"
+          : props.status === "Tentative"
+          ? "card agentDashBoardLatestUpdates-cards cards my-1 text-bg-secondary"
+          : props.status === "Confirmed"
+          ? "card agentDashBoardLatestUpdates-cards my-1 text-bg-success"
+          : "card agentDashBoardLatestUpdates-cards my-1 text-bg-danger"
+      }
+    >
       <div className="card-body">
-        <div className='d-flex justify-content-between align-items-center'>
+        <div className="d-flex justify-content-between align-items-center">
           <h5>{props.status}</h5>
           <h6>{props.fullName}</h6>
         </div>
-        <div className='d-flex justify-content-between align-items-center '>
+        <div className="d-flex justify-content-between align-items-center ">
           <h5 className="card-title">{props.destination}</h5>
-        <p className='fst-italic'>{props.travelDate}</p>
+          <p className="fst-italic">{props.travelDate}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AgentDashBoardLatestUpdates
+export default AgentDashBoardLatestUpdates;
--- client/src/pages/Agent/AgentDashboard/AgentDashboard.jsx
+++ client/src/pages/Agent/AgentDashboard/AgentDashboard.jsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
-import AgentDashBoardStatuses from './AgentDashComponents/AgentDashBoardStatuses'
-import AgentDashBoardLatestUpdates from './AgentDashComponents/AgentDashBoardLatestUpdates'
-import './Agentdashboard.css'
-import AgentDashBoardLineChart from './AgentDashComponents/AgentDashBoardLineChart'
-import AgentDashBoardStatusSortFilter
-from './AgentDashComponents/AgentDashBoardStatusSortFilter'
-import AgentDashBoardRadarChart from './AgentDashComponents/AgentDashBoardRadarChart'
+import React, { useState } from "react";
+import AgentDashBoardStatuses from "./AgentDashComponents/AgentDashBoardStatuses";
+import AgentDashBoardLatestUpdates from "./AgentDashComponents/AgentDashBoardLatestUpdates";
+import "./Agentdashboard.css";
+import AgentDashBoardLineChart from "./AgentDashComponents/AgentDashBoardLineChart";
+import AgentDashBoardStatusSortFilter from "./AgentDashComponents/AgentDashBoardStatusSortFilter";
+import AgentDashBoardRadarChart from "./AgentDashComponents/AgentDashBoardRadarChart";
 
 const AgentDashboard = (props) => {
-  
   const [sortType, setSortType] = useState("newest");
   const [selectedStatus, setSelectedStatus] = useState("All");
 
@@ -26,66 +24,89 @@ const AgentDashboard = (props) => {
       : travels.filter((travel) => travel.status === selectedStatus);
   };
 
-
   return (
     <>
-    <AgentDashBoardStatuses
+      <div className="col-xxl-10 col-xl-9 col-lg-9 ms-auto">
+        <AgentDashBoardStatuses
           statusesNumber={props.statusesNumber}
           estatus={props.estatus}
-          number={props.number} />
-    <section>
-        <div className='container-fluid'>
-            <div className='row p-2 align-items-center'>
-                <div className='col-xxl-10 col-xl-9 col-lg-9 ms-auto'>
-                    <div className='row' >
-                      <div className='col-xxl-6 col-xl-6 col-12 mb-4'>
-                        <div>
-                          <div className='d-flex justify-content-between align-items-center'>
-                            <h3 className='latest-update__heading text-dark'>Latest Updates</h3>
-                            <AgentDashBoardStatusSortFilter 
-                            selectedStatus={selectedStatus}
-                            setSelectedStatus={setSelectedStatus}
-                            sortType={sortType}
-                            setSortType={setSortType}
-                            />
-                          </div>
-                          <div className='overflow-auto agentDashboard-latest__updates p-1'>
-                                { 
-                                  
-                                  sortTravels(
-                                    filteredTravels
-                                    (props.travelDetails, selectedStatus), sortType
-                                      ).map((traveldetail, idx) => <AgentDashBoardLatestUpdates
-                                  key={idx}
-                                  fullName={traveldetail.fullName}
-                                  destination={traveldetail.destination}
-                                  travelDate={traveldetail.travelDate}
-                                  status={traveldetail.status}
-                                  />)
-                                  
-                                }
-                          </div>
-                        </div>    
-                      </div>
-                      <AgentDashBoardLineChart
-                      month={props.month}
-                      dummyChart={props.DUMMY_CHART}
-                      cosmopolitanLights={props.cosmopolitanLights}
-                      diveUnderWater={props.diveUnderWater}
-                      exploreTheSummit={props.exploreTheSummit}
-                      lookBackInHistory={props.lookBackInHistory}
-                      natureAndCulture={props.natureAndCulture}
-                      />        
-                    </div>
-                    <div className='row'>
-                      <AgentDashBoardRadarChart />
-                    </div>
-                </div>
+          number={props.number}
+        />
+        <div className="container-fluid px-4">
+          <div className="row mt-lg-2 mb-2">
+            <div className="col-xxl-6 col-xl-6 col-12">
+              <h4
+                className="mb-0 fw-bolder text-muted fs-3 mt-3 mb-1"
+                style={{
+                  backgroundColor: "var(--clr-secondary-000)",
+                  textAlign: "center",
+                }}
+              >
+                Latest Updates
+              </h4>
+              <div className="d-flex justify-content-between align-items-center">
+                <AgentDashBoardStatusSortFilter
+                  selectedStatus={selectedStatus}
+                  setSelectedStatus={setSelectedStatus}
+                  sortType={sortType}
+                  setSortType={setSortType}
+                />
+              </div>
+              <div className="overflow-auto agentDashboard-latest__updates p-1">
+                {sortTravels(
+                  filteredTravels(props.travelDetails, selectedStatus),
+                  sortType
+                ).map((traveldetail, idx) => (
+                  <AgentDashBoardLatestUpdates
+                    key={idx}
+                    fullName={traveldetail.fullName}
+                    destination={traveldetail.destination}
+                    travelDate={traveldetail.travelDate}
+                    status={traveldetail.status}
+                  />
+                ))}
+              </div>
             </div>
+            <div
+              className="col-xxl-6 col-xl-6 col-12"
+              style={{ height: "50vh" }}
+            >
+              <h4
+                className="fw-bolder text-muted fs-3 mt-3 mb-5"
+                style={{
+                  backgroundColor: "var(--clr-secondary-000)",
+                  textAlign: "center",
+                }}
+              >
+                Monthly Bookings
+              </h4>
+              <AgentDashBoardLineChart
+                month={props.month}
+                dummyChart={props.DUMMY_CHART}
+                cosmopolitanLights={props.cosmopolitanLights}
+                diveUnderWater={props.diveUnderWater}
+                exploreTheSummit={props.exploreTheSummit}
+                lookBackInHistory={props.lookBackInHistory}
+                natureAndCulture={props.natureAndCulture}
+              />
+            </div>
+          </div>
+          <div className="mt-lg-2 mt-2 col-xxl-6 col-xl-6 col-12 mx-auto">
+            <h4
+              className="fw-bolder text-muted fs-3 mt-4 mb-3"
+              style={{
+                backgroundColor: "var(--clr-secondary-000)",
+                textAlign: "center",
+              }}
+            >
+              Destinations Performance Summary
+            </h4>
+            <AgentDashBoardRadarChart />
+          </div>
         </div>
-    </section>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AgentDashboard
+export default AgentDashboard;
