8f0c0481b5e6370b86a4ab9232ec49982ade3fc7
Quen DP <delpilarquen@gmail.com>
2023-03-02 11:11:43 +0800

    #28-11 Add UserAuthentication handle sign up and log in function

--- client/src/context/UserAuthentication/UserAuthentication.jsx
+++ client/src/context/UserAuthentication/UserAuthentication.jsx
@@ -1,4 +1,10 @@
-import React, { createContext, useEffect, useMemo, useState } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import LogInForm from "./LogInForm/LogInForm";
 import LogOutForm from "./LogOutForm/LogOutForm";
 import SignUpForm from "./SignUpForm/SignUpForm";
@@ -8,25 +14,36 @@ const UserAuthContext = createContext({});
 export const UserAuthentication = ({ children }) => {
   // For development purposes remove in production
 
-  // const MOCK_LOGIN = {
-  //   token: "sampleToken",
-  //   username: "sample@username",
-  //   role: "user",
-  // };
-
   const MOCK_LOGIN = {
+    token: "sampleToken",
+    username: "sample@username",
+    role: "user",
+  };
+
+  const MOCK_LOGOUT = {
     token: false,
     username: "login",
     role: false,
   };
 
+  console.log("Mock data log in : ", MOCK_LOGIN);
+  console.log("Mock data log out : ", MOCK_LOGOUT);
+
   // For development purposes remove in production
 
   const [hasAccount, setHasAccount] = useState(true);
-  const [logInToken, setLogInToken] = useState(MOCK_LOGIN);
+  const [logInToken, setLogInToken] = useState(MOCK_LOGOUT);
 
   const handleChangeMode = () => {
     hasAccount === true ? setHasAccount(false) : setHasAccount(true);
+  };
+
+  const handleLogInMode = () => {
+    setHasAccount(true);
+  };
+
+  const handleSignUpMode = () => {
+    setHasAccount(false);
   };
 
   const submitHandler = (jwtToken, username, role) => {
